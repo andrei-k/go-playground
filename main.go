331a8bd3eb617758cb4f84f2d875ec3c56e4021b
Andrei K <andrei@12ak.com>
2022-03-28 21:09:00 -0400

    Get Eliza program to respond and quit on user input

--- main.go
+++ main.go
@@ -1,14 +1,35 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"myapp/doctor"
+	"os"
+	"strings"
 )
 
 func main() {
-	var whatToSay string
-
-	whatToSay = doctor.Intro()
-
+	reader := bufio.NewReader(os.Stdin)
+	whatToSay := doctor.Intro()
 	fmt.Println(whatToSay)
+
+	for {
+		fmt.Print("-> ")
+
+		// Capture user input until the return character is pressed
+		userInput, _ := reader.ReadString('\n')
+
+		// Replace return input on Windows
+		userInput = strings.Replace(userInput, "\r\n", "", -1)
+		// Replace return input on Mac and Linux
+		userInput = strings.Replace(userInput, "\n", "", -1)
+		// (-1 indicates to replace everywhere the match it occurs)
+
+		if userInput == "quit" {
+			break
+		} else {
+			response := doctor.Response(userInput)
+			fmt.Println(response)
+		}
+	}
 }
