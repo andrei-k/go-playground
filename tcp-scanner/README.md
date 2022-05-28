20efcf06f11babac50f170a31c3f69547dcdad3c
Andrei K <andrei@12ak.com>
2022-05-28 13:44:51 -0400

    Improve comments

--- tcp-scanner/README.md
+++ tcp-scanner/README.md
@@ -4,6 +4,8 @@ Scan all 65,535 ports on localhost using Goroutines, a Worker Pool, and multicha
 
 A Worker Pool is used to avoid inconsistencies due to network or system limitations when scanning a large number of ports simultaneously.
 
+This is from the lesson in the **Black Hat Go** book.
+
 ## Usage
 
 Run in Terminal
--- tcp-scanner/main.go
+++ tcp-scanner/main.go
@@ -7,6 +7,7 @@ import (
 )
 
 func worker(ports, results chan int) {
+	// Range to continuously receive from the ports channel, looping until the channel is closed
 	for p := range ports {
 		address := fmt.Sprintf("127.0.0.1:%d", p)
 		conn, err := net.Dial("tcp", address)
@@ -22,22 +23,30 @@ func worker(ports, results chan int) {
 
 func main() {
 	// Create a buffered channel with a capacity of 100
+	// This will be used to create 100 workers
 	ports := make(chan int, 100)
+
+	// Use a separate channel to pass the result of the port scan back to the main thread
 	results := make(chan int)
+
+	// Use a slice to store the results to sort it later
 	var openPorts []int
 
 	fmt.Println("Start scanning...")
 
+	// Start a desired number of workers, in this case it's 100
 	for i := 0; i <= cap(ports); i++ {
 		go worker(ports, results)
 	}
 
+	// Sending all port values to the workers as a separate goroutine so the result-gathering loop below can start asynchronously
 	go func() {
 		for i := 1; i <= 65535; i++ {
 			ports <- i
 		}
 	}()
 
+	// Because the number of ports sent and the number of results received are the same, the program can know when to close the channels and subsequently shut down the workers
 	for i := 0; i < 65535; i++ {
 		port := <-results
 		if port != 0 {
