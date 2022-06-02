ac455e4f0334d85b2ac2891379eac51722973594
Andrei K <andrei@12ak.com>
2022-06-01 22:40:11 -0400

    Add mutex

---
+++ mutex/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+// Counter struct that also holds the mutex used to lock/unlock the counter variable so it can be used by one goroutine instance at a time
+type Counter struct {
+	mutex   sync.Mutex
+	counter int
+}
+
+// This is a bad example where the counter will likely be out of order due to non-deterministic interleaving
+func (c *Counter) increment1(wg *sync.WaitGroup) {
+	c.counter++
+	fmt.Println("x:", c.counter)
+	wg.Done()
+}
+
+// The counter will be in order because of the mutex
+func (c *Counter) increment2(wg *sync.WaitGroup) {
+	c.mutex.Lock()
+	c.counter++
+	fmt.Println("x:", c.counter)
+	wg.Done()
+	c.mutex.Unlock()
+}
+
+func main() {
+	var wg sync.WaitGroup
+	c := Counter{}
+
+	wg.Add(10)
+	for i := 0; i < 10; i++ {
+		go c.increment1(&wg)
+	}
+	wg.Wait()
+
+	fmt.Println("-------------------")
+	c = Counter{}
+
+	wg.Add(10)
+	for i := 0; i < 10; i++ {
+		go c.increment2(&wg)
+	}
+	wg.Wait()
+}
