9fe0de2e7c3d6c65e5877ffe43c2f948ef2bce7d
Andrei K <andrei@12ak.com>
2022-06-02 17:31:41 -0400

    Messing around

--- dining-philosophers/main.go
+++ dining-philosophers/main.go
@@ -2,8 +2,29 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
+var wg sync.WaitGroup
+
+func doStuff(ch1, ch2 chan int) {
+	<-ch1    // Wait for the first channel
+	ch2 <- 1 // Write to the second channel
+	wg.Done()
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+
+	// for i := 0; i < 10; i++ {
+	wg.Add(2)
+	// This will result in a deadlock because the channels will be waiting for each other in the goroutine
+	go doStuff(ch1, ch2)
+	go doStuff(ch2, ch1)
+	msg1 := <-ch1
+	msg2 := <-ch2
+	fmt.Println("m1:", msg1, "m2:", msg2)
+	// }
+	wg.Wait()
 }
