b763e631cd0ae70fb3883ee2c66b00c4311efe28
Andrei K <andrei@12ak.com>
2024-02-07 21:07:26 -0500

    Add logic

--- graph-plotter/README.md
+++ graph-plotter/README.md
@@ -1,9 +1,17 @@
 # Graph plotter
 
 ## Reminders for writing a Go project
-Navigate to project folder and run in terminal:
+Navigate to project folder
+
+Initialize the project by running this command in the terminal:
 ```
 go mod init graph-plotter
+```
+
+Install the Gonum plot library
+
+```
+go get -u gonum.org/v1/plot/...
 ```
 
 ## Usage
--- graph-plotter/main.go
+++ graph-plotter/main.go
@@ -1,47 +1,63 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
-	"strings"
+	"log"
+	"sort"
+
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
 )
 
 func main() {
-	fmt.Println("Enter in a sequence of integers separated by spaces")
-	fmt.Print("-> ")
-
-	reader := bufio.NewReader(os.Stdin)
-	input, _ := reader.ReadString('\n')
-	// Strip off the carriage return
-	input = strings.Replace(input, "\r\n", "", -1) // Windows
-	input = strings.Replace(input, "\n", "", -1)   // Mac
-
-	strings := strings.Split(input, " ")
-
-	ints := make([]int, len(strings))
-	for i, v := range strings {
-		ints[i], _ = strconv.Atoi(v)
+	// Define the data
+	data := map[int]float64{
+		2017: 21828,
+		2018: 30340,
+		2019: 35139,
+		2020: 33978,
+		2021: 36744,
+		2022: 41283,
+		2023: 44251,
 	}
 
-	BubbleSort(ints)
-	fmt.Println(ints)
-}
-
-// BubbleSort() takes a slice of integers as an argument and returns nothing It modifies the slice so that the elements are in sorted order.
-func BubbleSort(sli []int) {
-	for i := 0; i < len(sli)-1; i++ {
-		for j := 0; j < len(sli)-1-i; j++ {
-			if sli[j] > sli[j+1] {
-				Swap(sli, j)
-			}
-		}
+	// Extract x (years) and y (totals) values from the data map
+	var years []int
+	var totals []float64
+	for year, total := range data {
+		years = append(years, year)
+		totals = append(totals, total)
 	}
-}
 
-// Swap() function takes a slice of integers and an index value i which indicates a position in the slice.
-// The function should return nothing, but it should swap the contents of the slice in position i with the contents in position i+1.
-func Swap(sli []int, i int) {
-	sli[i], sli[i+1] = sli[i+1], sli[i]
+	// Sort the years
+	sort.Ints(years)
+
+	// Create a new plot
+	p := plot.New()
+
+	// Create a line plotter
+	line, err := plotter.NewLine(plotter.XYs{})
+	if err != nil {
+		log.Fatalf("Could not create line plotter: %v", err)
+	}
+
+	// Add the data to the line plotter
+	for i, year := range years {
+		line.XYs = append(line.XYs, struct{ X, Y float64 }{float64(year), totals[i]})
+	}
+
+	// Add the line plotter to the plot
+	p.Add(line)
+
+	// Set the title and labels
+	p.Title.Text = "Totals over years"
+	p.X.Label.Text = "Year"
+	p.Y.Label.Text = "Totals"
+
+	// Save the plot to a file
+	if err := p.Save(8*vg.Inch, 4*vg.Inch, "plot.png"); err != nil {
+		log.Fatalf("Could not save plot: %v", err)
+	}
+	fmt.Println("Plot saved to plot.png")
 }
